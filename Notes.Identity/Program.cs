528d969383d6e07d34d00a1785882868fe06b6b6
eeestrany <yaroslavprotsyk310@gmail.com>
2023-07-31 14:12:27 +0300

    Add project files.

---
+++ Notes.Identity/Program.cs
@@ -0,0 +1,80 @@
+using Microsoft.AspNetCore.Mvc;
+using IdentityServer4.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Notes.Identity;
+using Notes.Identity.Data;
+using Notes.Identity.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var appConfiguration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .Build();
+
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+{
+    options.UseSqlServer(appConfiguration.GetConnectionString("DbConnection"));
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(config =>
+{
+    config.Password.RequiredLength = 4;
+    config.Password.RequireDigit = false;
+    config.Password.RequireLowercase = false;
+    config.Password.RequireUppercase = false;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<AppUser>()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
+builder.Services.ConfigureApplicationCookie(confige =>
+{
+    confige.Cookie.Name = "Notes.Identity.Cookie";
+    confige.LoginPath = "/Auth/Login";
+    confige.LogoutPath = "/Auth/Logout";    
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var serviceProvider = scope.ServiceProvider;
+    try
+    {
+        var context = serviceProvider.GetRequiredService<AuthDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception ex) { }
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
